77d70d30b1530e28b50a30355ffa8a931a27af60
Stepan Tanasiychuk <stepan.tanasiychuk@gmail.com>
2013-01-22 10:28:23 +0200

    finished work on the movement of the tank. added new textures and players images

--- battle-city-v3/controller.js
+++ battle-city-v3/controller.js
@@ -1,66 +1,108 @@
 /** @class BattleCity.Controller */
 atom.declare( 'BattleCity.Controller', {
+
+    texturesFields: [],
+
     initialize: function () {
         atom.ImagePreloader.run({
-            player : 'tank.png',
+            player : 'tank.png [64:32]{0:0}',
+            player2 : 'tank.png [64:32]{1:0}',
+            player3 : 'tank.png [64:32]{2:0}',
+            player4 : 'tank.png [64:32]{3:0}',
             textures : 'textures.png'
         }, this.start.bind(this));
+
+        this.fpsMeter();
     },
 
     start: function (images) {
+        this.game = new BattleCity.Game(this);
+        atom.frame.add(this.game.update);
+
         this.size = new Size(416, 416);
+
         this.app = new App({ size: this.size });
 
         // images ready
         this.app.resources.set('images', images);
 
-        // отдельный слой для текстур, который будем перерисовывать только по команде
+        // слой для фона и нижних текстур (вода и асфальт)
+        this.background = this.app.createLayer({
+            name: 'background',
+            intersection: 'manual',
+            zIndex: 1
+        });
+        this.background.ctx.fillAll('black');
+
+        // слой для верхних текстур
         this.textures = this.app.createLayer({
             name: 'textures',
             intersection: 'manual',
-            zIndex: 2
+            zIndex: 3
         });
 
         // слой для юнитов, перерисовываем постоянно
         this.units = this.app.createLayer({
             name: 'units',
             invoke: true,
-            zIndex: 3
+            zIndex: 2
         });
 
         // игрок
         this.player = new BattleCity.Player(this.units, {
             size: this.size,
             controls: { up: 'aup', down: 'adown', left: 'aleft', right: 'aright' },
-            images: images
+            images: images,
+            controller: this
         });
+
+        // координатная сетка (для дебага)
+//        for (var y = 0; y < 52; y++) {
+//            for (var x = 0; x < 52; x++) {
+//                this.textures.ctx.fillStyle   = 'red'; // blue
+//                this.textures.ctx.fillRect(x*8, y*8, 1, 1);
+//            }
+//        }
 
         // построение уровня
         var data = this.constructor.levels[0];
         for (var y = 0; y < 26; y++) {
             s = data[y];
             for (var x = 0; x < 26; x++) {
+                var textureField = null;
                 var rectangle = new Rectangle(x*16, y*16, 16, 16);
                 switch(s.charAt(x)) {
                     case '#':
-                        new BattleCity.Wall(this.textures, {
+                        textureField = new BattleCity.Wall(this.textures, {
                             shape: rectangle
                         });
                     case '=':
-                        new BattleCity.Breaks(this.textures, {
+                        textureField = new BattleCity.Breaks(this.textures, {
                             shape: rectangle
                         });
                         break;
                     case '*':
-                        new BattleCity.Trees(this.textures, {
+                        textureField = new BattleCity.Trees(this.textures, {
+                            shape: rectangle
+                        });
+                        break;
+                    case '~':
+                        textureField = new BattleCity.Water(this.background, {
+                            shape: rectangle
+                        });
+                        break;
+                    case '>':
+                        textureField = new BattleCity.Asphalt(this.background, {
                             shape: rectangle
                         });
                         break;
                 }
+
+                if (textureField) {
+                    this.texturesFields.push(textureField);
+                }
             }
         }
-
-        this.fpsMeter();
     },
 
     fpsMeter: function () {
---
+++ battle-city-v3/game.js
@@ -0,0 +1,41 @@
+/** @class BattleCity.Game */
+atom.declare('BattleCity.Game', {
+
+    initialize: function (controller) {
+//        this.bindMethods('update');
+
+        this.controller = controller;
+        this.players = [];
+        this.textures = [];
+//        this.bullets = [];
+//        this.enemies = [];
+    },
+
+    /** @private */
+    getArray: function (item) {
+        var a = item instanceof BattleCity.Wall ? this.textures :
+//            item instanceof BattleCity.Bullet ? this.bullets :
+//            item instanceof BattleCity.Enemy ? this.enemies :
+            item instanceof BattleCity.Player ? this.players : null;
+
+        if (a == null) throw new TypeError( 'unknown type of ' + item );
+
+        return a;
+    },
+
+    add: function (item) {
+        this.getArray(item).push(item);
+        return this;
+    },
+
+    remove: function (item) {
+        this.getArray(item).erase(item);
+        return this;
+    },
+
+    update: function () {
+        console.log(new Date().getTime());
+//        this.shipsAsteroids();
+//        this.bulletsAsteroids();
+    },
+});
